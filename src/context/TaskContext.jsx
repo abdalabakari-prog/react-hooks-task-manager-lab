c654ca72dcffd543e7121defa1ab525ec9d26b69
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-01-18 11:37:11 +0300

    Update TaskContext.jsx

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,6 +1,48 @@
-import React, { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const TaskContext = createContext();
 
 export function TaskProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/tasks")
+      .then((res) => res.json())
+      .then((data) => setTasks(data));
+  }, []);
+
+  const addTask = (newTask) => {
+    fetch("http://localhost:3001/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newTask),
+    })
+      .then((res) => res.json())
+      .then((data) => setTasks([...tasks, data]));
+  };
+
+  const toggleComplete = (id) => {
+    const task = tasks.find((t) => t.id === id);
+    const updatedTask = { ...task, completed: !task.completed };
+
+    fetch(`http://localhost:3001/tasks/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ completed: updatedTask.completed }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setTasks(tasks.map((t) => (t.id === id ? data : t)));
+      });
+  };
+
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, toggleComplete }}>
+      {children}
+    </TaskContext.Provider>
+  );
 }
